bbe2dbd5d13e7f8930f9a14da8d86b7b45247005
Eric Coleman <ericcoleman9217@gmail.com>
2022-01-08 17:43:09 -0600

    added login route and password verification

--- models/User.js
+++ models/User.js
@@ -1,8 +1,14 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // Create our User model
-class User extends Model {}
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw){
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 // define table columns and configuration
 User.init(
@@ -47,6 +53,19 @@ User.init(
         
     },
     {
+        hooks: {
+            // Set up beforeCreate lifecycle 'hook' functionality
+            async beforeCreate(newUserData){
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            // Set up beforeUpdate 'hook' functionality
+            async beforeUpdate(updatedUserData){
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+
+        },
         // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
     
         // pass in our imported sequelize connection (the direct connection to our database)
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -53,12 +53,36 @@ router.post('/', (req, res) => {
     });
 });
 
+// POST api/user/login
+router.post('/login', (req,res) => {
+    // Expects {email: 'lernantino@email.com', password: 'password1234'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if(!dbUserData){
+            res.status(400).json({ message: 'No user with that email found'});
+            return;
+        }
+        //res.json({ user: dbUserData });
+        // verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if(!validPassword){
+            res.status(400).json({ message: 'Incorrect Password' });
+            return;
+        }
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+});
 // PUT /api/users/1
 router.put('/:id', (req,res) => {
     // expects {username: 'Lernantino', email: 'lernantino@email.com', password: 'password1234'}
 
-    // if req.body has exact key/value pairs to match the model, you can just use` `req.body` instead 
+    // pass in req.body instead to only update what's passed through
     User.update(req.body, {
+        individualHooks: 
+            true,
         where: {
             id: req.params.id
         }
