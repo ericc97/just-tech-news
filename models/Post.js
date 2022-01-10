39c72bb483f8c434a2bb0cfdec8489cfcbf3861b
Eric Coleman <ericcoleman9217@gmail.com>
2022-01-10 14:58:47 -0600

    added Post model

---
+++ models/Post.js
@@ -0,0 +1,50 @@
+const { underscore } = require('inflection');
+const { Model, DataTypes } = require('sequelize');
+const { User } = require('./User');
+const sequelize = require('../config/connection');
+
+// Create our post model
+class Post extends Model {}
+
+// Create fields /columns for post model 
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+             
+            type: DataTypes.STRING,
+            allowNull: false
+            
+        },
+        post_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isURL: true
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+    
+
+);
+
+module.exports = Post;
