be6fcf7eacc34dcd96e6cd7bb1edd89908199621
Eric Coleman <ericcoleman9217@gmail.com>
2022-01-11 15:58:25 -0600

    added upvote to post routes

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
 
 // Create associations
@@ -11,6 +12,34 @@ Post.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_key'
+});
+
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
 
 
-module.exports = { User, Post };
+
+module.exports = { User, Post, Vote };
